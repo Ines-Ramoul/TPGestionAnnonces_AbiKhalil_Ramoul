a2912d9a2e514e53bfb306cf33f1852c696241a7
Inès Ramoul <ines.ramoul@etu.unice.fr>
2019-10-01 11:41:55 +0200

    generate controllers and views from domains

--- grails-app/domain/tpgestionannonces/User.groovy
+++ grails-app/domain/tpgestionannonces/User.groovy
@@ -2,6 +2,15 @@ package tpgestionannonces
 
 class User {
 
+    String username
+    String password
+    //ajout du Validator sur le champ password sur les contraintes, sinon verif au niveau Metier
+    Date dateCreated
+    Date lastUptated
+    Illustration thumbnail
+
+    static hasMany = [annonces : Annonce]
+
     static constraints = {
         username nullable : false, blank: false, size : 5..20
         password password : true, nullable : false, blank : false, size: 8..30
@@ -9,12 +18,6 @@ class User {
         annonces nullable : true
     }
 
-    static hasMany = [annonces : Annonce]
 
-    String username
-    String password
-    //ajout du Validator sur le champ password sur les contraintes, sinon verif au niveau Metier
-    Date dateCreated
-    Date lastUptated
-    Illustration thumbnail
+
 }
--- grails-app/init/tpgestionannonces/BootStrap.groovy
+++ grails-app/init/tpgestionannonces/BootStrap.groovy
@@ -3,11 +3,30 @@ package tpgestionannonces
 class BootStrap {
 //init nous sert à repeupler notre base à chaque démarrage du serveur d'application
     def init = { servletContext ->
-        new User(username : "username",
-                password : "password",
-                thumbnail: new Illustration(filename: "thumb.png"))
-                .save(flush:true,failOnError:true)
+
+        def userInstance = new User(username: "username",
+                password: "password",
+                thumbnail: new Illustration(filename: "thumb.png")
+        ).save(flush: true, failOnError: true)
+
+        (1..5).each {
+                    def annonceInstance = new Annonce(
+                            title: "title",
+                            description: "description",
+                            validTill: new Date(),
+                            state: Boolean.TRUE
+                    )
+                            .addToIllustrations(new Illustration(filename: "filename"))
+                            .addToIllustrations(new Illustration(filename: "filename_2"))
+                            .addToIllustrations(new Illustration(filename: "filename_3"))
+            userInstance.addToAnnonces(annonceInstance)
+
+        }
+        userInstance.save(flush: true, failOnError: true)
     }
+
+
     def destroy = {
     }
+
 }
