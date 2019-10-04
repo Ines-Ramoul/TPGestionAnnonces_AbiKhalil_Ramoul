993ecb61ac3befe8c1f5887460a8b6ec59d87860
CHARBELAK\Charbel <charbel.abi-khalil@etu.unice.fr>
2019-10-04 12:36:55 +0200

    cron job DONE

---
+++ grails-app/jobs/tpgestionannonces/DeleteInvalidAnnonceJob.groovy
@@ -0,0 +1,18 @@
+package tpgestionannonces
+
+class DeleteInvalidAnnonceJob {
+    static triggers = {
+      simple repeatInterval: 20000l // execute job once in 5 seconds
+    }
+
+
+    def execute() {
+        println("INSIDE")
+        def annonceInstance = Annonce.find ("from Annonce as a where a.validTill < :nowDate",[nowDate: new Date()])
+        annonceInstance.each {
+            println("CIAOOO "+it.id)
+            it.delete(flush: true)
+        }
+        // execute job
+    }
+}
